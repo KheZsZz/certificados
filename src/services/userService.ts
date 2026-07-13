b7655e25bd81fb21336f7e8a63ab2b8eb5129f45
kevin Oliveira <kevinklgvg@gmail.com>
2026-07-13 15:24:44 -0300

    create initial user service

---
+++ src/services/userService.ts
@@ -0,0 +1,31 @@
+import { prisma } from '../lib/prisma'
+import { UserCreateInput } from '@/generated/models'
+import { hash } from 'bcrypt'
+
+export const userService = {
+  create: async (data: UserCreateInput) => {
+    const emailExists = await prisma.user.findUnique({
+      where: { email: data.email }
+    })
+
+    if (emailExists) {
+      throw new Error('E-mail already in use.')
+    }
+
+    const hashedPassword = await hash(data.password, 8) //encrypt password
+
+    const newUser = await prisma.user.create({
+      data: {
+        ...data,
+        password: hashedPassword
+      },
+
+      select: {
+        name: true,
+        role: true,
+      }
+    })
+
+    return newUser
+  }
+}
