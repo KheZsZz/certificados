590582bb638b7fbf151480fd9ae958e5e60ddef2
KEvin <kevinklgvg@gmail.com>
2026-07-13 23:05:09 -0300

    alter roles users

--- src/configs/roles.ts
+++ src/configs/roles.ts
@@ -1,8 +1,8 @@
 export const ROLE_HIERARCHY: Record<string, number> = {
-  STUDENT: 1,
-  TEACHER: 2,
-  SECRETARY: 3,
-  FINANCER: 4,
-  ADM: 5,
   DEVELOPERMENT: 6,
+  ADM: 5,
+  FINANCER: 4,
+  SECRETARY: 3,
+  TEACHER: 2,
+  STUDENT: 1,
 }
