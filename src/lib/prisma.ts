06b197d340f479f654cfaa2dad2aa6b96b6878da
KEvin <kevinklgvg@gmail.com>
2026-07-13 23:05:17 -0300

    alter client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-export const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL
-});
+import { PrismaClient } from "@/generated/client"
+
+
+const connectionString = process.env.DATABASE_URL
+
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+
+
+export const prisma = new PrismaClient({ adapter })
